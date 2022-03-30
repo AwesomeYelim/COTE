007fe01e1aa7088112a1ea14a2b695c30cba0ae7
hongyelim <uiop01900@gmail.com>
2022-03-30 11:02:42 +0900

    74

--- js100/js_100.js
+++ js100/js_100.js
@@ -1713,33 +1713,92 @@
 // X (못품 -> 블로그 정리하면서 다시 정리)
 
 // 문제 73. 최단 경로 찾기
+// let graph = {
+//   A: ["B", "C"],
+//   B: ["A", "D", "E"],
+//   C: ["A", "F"],
+//   D: ["B"],
+//   E: ["B", "F"],
+//   F: ["C", "E"],
+// };
+
+// function 최단경로(graph, start, end) {
+//   let count = -1;
+//   let 거쳐감 = [start];
+//   let 최종 = [start];
+
+//   while (거쳐감.length !== 0) {
+//     count += 1;
+//     let s = 거쳐감.length;
+//     for (let i = 0; i < s; i++) {
+//       let n = 거쳐감.splice(0, 1);
+//       if (n == end) {
+//         return count;
+//       }
+//       for (let j in graph[n]) {
+//         if (!최종.includes(graph[n][j])) {
+//           최종.push(graph[n][j]);
+//           거쳐감.push(graph[n][j]);
+//         }
+//       }
+//     }
+//   }
+// }
+
+// // let 입력 = prompt('입력쓰').split(' ');
+// let 입력 = ["A", "F"];
+// let start = 입력[0];
+// let end = 입력[1];
+
+// console.log(최단경로(graph, start, end));
+
+// X (위에꺼 못풀어서 못품)
+
+// 문제 74. 최장 경로 찾기
+
 let graph = {
-  A: ["B", "C"],
-  B: ["A", "D", "E"],
-  C: ["A", "F"],
-  D: ["B"],
-  E: ["B", "F"],
-  F: ["C", "E"],
+  1: [2, 3, 4],
+  2: [1, 3, 4, 5, 6],
+  3: [1, 2, 7],
+  4: [1, 2, 5, 6],
+  5: [2, 4, 6, 7],
+  6: [2, 4, 5, 7],
+  7: [3, 5, 6],
 };
 
-function 최단경로(graph, start, find) {
-  let visited = [];
-  let first = [start];
+// let 입력 = prompt('입력쓰').split(' ');
+let 입력 = ["1", "7"];
+let start = Number(입력[0]);
+let end = Number(입력[1]);
 
-  while (first.length !== 0) {
-    let n = first.pop();
-    if (!visited.includes(n)) {
-      visited.push(n);
-      let m = graph[n].filter((x) => !visited.includes(x));
-      for (let i of m) {
-        first.push(i);
-      }
-    }
+let queue = [start];
+let visited = [];
+
+function 최장거리(q, visited) {
+  let node = q[q.length - 1];
+  let length = 0;
+
+  if (node == end) {
+    return visited.length;
   }
+  if (visited.includes(node)) {
+    return visited.length;
+  } else {
+    visited.push(node);
+  }
+  let max = [];
 
-  return visited.indexOf(find) - visited.indexOf(start);
+  for (let i in graph[node]) {
+    q.push(graph[node][i]);
+    max.push(length, 최장거리(q, visited));
+
+    length = Math.max.apply(null, max);
+
+    queue.pop();
+  }
+  return length;
 }
 
-console.log(최단경로(graph, "A", "F"));
+console.log(최장거리(queue, visited));
 
-// O (깊이 우선 탐색으로 품 -> 다시한번 풀기)
+//  X
