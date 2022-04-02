a0e656dc3046ca46e1878a5b90ca085e7df11fdd
hongyelim <uiop01900@gmail.com>
2022-04-02 20:41:27 +0900

    오답66

--- js100/오답노트/연습장.js
+++ js100/오답노트/연습장.js
@@ -1,3 +1,25 @@
-let 탑 = ['ABCDEF', 'BCAD', 'ADEFQRX', 'BEDFG', 'EFGHZ']
-let 규칙 = 'ABD'.split('')
-let arr = []
+const 전체블록 = ["ABCDEF", "BCAD", "ADEFQRX", "BEDFG", "EFGHZ"];
+let 규칙 = "ABD";
+
+function solution(전체블록, 규칙) {
+  let answer = [];
+  for (let 부분블록 of 전체블록) {
+    answer.push(블록순서체크(부분블록, 규칙)); //콜백함수를 넣어줌
+  }
+  return answer;
+}
+function 블록순서체크(부분블록, 규칙) {
+  let 임시변수 = 규칙.indexOf(규칙[0]);
+  for (let 문자 of 부분블록) {
+    console.log(문자)
+    if (규칙.includes(문자)) {
+      if (임시변수 > 규칙.indexOf(문자)) {
+        return "불가능";
+      }
+      임시변수 = 규칙.indexOf(문자);
+    }
+  }
+  return "가능";
+}
+
+console.log(solution(전체블록, 규칙));
