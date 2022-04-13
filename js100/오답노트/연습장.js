80a5b6fcd60f423ee5758090d03f48ab9940ab2b
hongyelim <uiop01900@gmail.com>
2022-04-13 13:40:55 +0900

    오답74

--- js100/오답노트/연습장.js
+++ js100/오답노트/연습장.js
@@ -1,77 +1,44 @@
-// let graph = {
-//   A: ["B", "C"],
-//   B: ["A", "D", "E"],
-//   C: ["A", "F"],
-//   D: ["B"],
-//   E: ["B", "F"],
-//   F: ["C", "E"],
-// };
-// let graph_k = Object.keys(graph)
-// // console.log(graph_k)
-
-// function rotate(start, end, graph) {
-//     let count = 0;
-
-//     for(let key of graph_k){
-//         let value = graph[key];
-//         // console.log(value)
-//         if(key === start){
-//             count += 1;
-//         }
-        
-//         for(let j in value){
-//             // console.log(value[j])
-//             if(value[j] === end){
-//                 return count;
-//             }
-//             if(value[j] !== end){
-//                 console.log(value[j])
-//             }
-//         }
-//     }
-// }
-// let 입력 = 'A F'.split(' ')
-// let start = 입력[0]
-// let end = 입력[1]
-
-// console.log(rotate(start, end, graph));
-
-
 let graph = {
-    A: ["B", "C"],
-    B: ["A", "D", "E"],
-    C: ["A", "F"],
-    D: ["B"],
-    E: ["B", "F"],
-    F: ["C", "E"],
-  };
-  
-  function 최단경로(graph, start, end) {
-    let count = -1;
-    let 거쳐감 = [start];
-    let 최종 = [start];
-  
-    while (거쳐감.length !== 0) {
-      count += 1;
-      let s = 거쳐감.length;
-      for (let i = 0; i < s; i++) {
-        let n = 거쳐감.splice(0, 1);
-        if (n == end) {
-          return count;
-        }
-        for (let j in graph[n]) {
-          if (!최종.includes(graph[n][j])) {
-            최종.push(graph[n][j]);
-            거쳐감.push(graph[n][j]);
-          }
-        }
-      }
-    }
+  1: [2, 3, 4],
+  2: [1, 3, 4, 5, 6],
+  3: [1, 2, 7],
+  4: [1, 2, 5, 6],
+  5: [2, 4, 6, 7],
+  6: [2, 4, 5, 7],
+  7: [3, 5, 6],
+};
+
+// let 입력 = prompt('입력쓰').split(' ');
+let 입력 = ["1", "7"];
+let start = Number(입력[0]);
+let end = Number(입력[1]);
+
+let queue = [start];
+let visited = [];
+
+function 최장거리(q, visited) {
+  let node = q[q.length - 1];
+  let length = 0;
+
+  if (node == end) {
+    return visited.length;
   }
-  
-  // let 입력 = prompt('입력쓰').split(' ');
-  let 입력 = ["A", "F"];
-  let start = 입력[0];
-  let end = 입력[1];
-  
-  console.log(최단경로(graph, start, end));
+  if (visited.includes(node)) {
+    return visited.length;
+  } else {
+    visited.push(node);
+  }
+  let max = [];
+
+  for (let i in graph[node]) {
+    q.push(graph[node][i]);
+    max.push(length, 최장거리(q, visited));
+
+    length = Math.max.apply(null, max);
+
+    queue.pop();
+  }
+  return length;
+}
+
+console.log(최장거리(queue, visited));
