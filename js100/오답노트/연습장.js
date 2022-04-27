bb4862994e71039e34d41132cd80cbf01cb41579
hongyelim <uiop01900@gmail.com>
2022-04-28 08:32:22 +0900

    오답95

--- js100/오답노트/연습장.js
+++ js100/오답노트/연습장.js
@@ -1,25 +1,77 @@
-// 내가 시도한 풀이
+let stmp = [
+  [1, 1, 1, 2],
+  [2, 0, 0, 0], 
+  [1, 1, 1, 1],
+  [0, 0, 0, 0],
+];
 
-// let point = [1, 1, 3, 2, 5];
-// 1, 1, 3, 2, 5
-// let dish = 3;//index
+let k = 1;
+  
+function sol(stmp, k) {
+  let n = stmp.length;
+  let p = [];
 
-const point = [5, 2, 3, 1, 2, 5];
-// 5, 2, 3, 1, 2, 5, 5, 2, 3, 5
-let dish = 1; //index
-
-function rotate1(p, d) {
-  let arr = [];
-  let sort = point.slice();
-  sort = sort.sort((a, b) => a - b);
-
-  rotate2(p, d);
-  return sort;
-}
-
-function rotate2(p, d) {
-  for (let i in p) {
-    
+  for (let i = 0; i < n; i++) {
+    p.push(Array(n).fill(0)); //0으로 채워진 n*n 배열을 만들어준다
   }
+  //   console.log(p)
+  // 회전시키기 전 한번 찍어주기
+  p = sum_matrix(p, stmp);
+
+  for (let i = 0; i < k; i++) {
+    stmp = rotate(stmp);
+    p = sum_matrix(p, stmp);
+  }
+
+  return p;
 }
-console.log(rotate1(point, dish));
+
+// 회전
+function rotate(stmp) {
+  let n = stmp.length;
+  let rot = [];
+
+  for (let i = 0; i < n; i++) {
+    rot.push(Array(n).fill(0));
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      rot[j][n - 1 - i] = stmp[i][j];
+    }
+  }
+  return rot;
+}
+
+//총합
+function sum_matrix(p, stmp) {
+  let n = stmp.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      p[i][j] += stmp[i][j];
+    }
+  }
+  return p;
+}
+console.log(sol(stmp, k));
+
+// i,j -> j,N-1-i
+// 0,0 -> 0,3
+// 0,1 -> 1,3
+// 0,2 -> 2,3
+// 0,3 -> 3,3
+
+// 1,0 -> 0,2
+// 1,1 -> 1,2
+// 1,2 -> 2,2
+// 1,3 -> 3,2
+
+// 2,0 -> 0,1
+// 2,1 -> 1,1
+// 2,2 -> 2,1
+// 2,3 -> 3,1
+
+// 3,0 -> 0,0
+// 3,1 -> 1,0
+// 3,2 -> 2,0
+// 3,3 -> 3,0
