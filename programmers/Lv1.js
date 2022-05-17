e47a6576896ccaeac8f9c5bac713605674b4be57
hongyelim <uiop01900@gmail.com>
2022-05-17 21:20:46 +0900

    lv1

--- programmers/Lv1.js
+++ programmers/Lv1.js
@@ -321,7 +321,6 @@
 
 // console.log(solution(numbers, hand));
 
-
 // // 다른사람풀이
 
 // function solution(numbers, hand) {
@@ -353,6 +352,16 @@
 // }
 
 // 문제 6. 크레인 인형뽑기 게임
+
+let board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+
+let moves = [1, 5, 3, 5, 1, 2, 1, 4];
 
 function solution(board, moves) {
   var answer = 0;
