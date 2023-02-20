9cefb51ce675f953e5045254d9f8c23902c9405c
AwesomeYelim <uiop01900@gmail.com>
2023-02-20 17:20:20 +0900

    008

--- programmers/Lv2/008.js
+++ programmers/Lv2/008.js
@@ -1,26 +1,36 @@
 // Lv2. 시소 짝꿍
 const weights = [100, 180, 360, 100, 270];
 
+// 시간초과 걸림
 function solution(weights) {
-  let include = [2, 3, 4];
-  let [count, left, right] = [0, 0, 0];
-  while (left < weights.length) {
-    // 최대공약수 구하기
-    const factor = (a, b) => {
-      return b === 0 ? a : factor(b, a % b);
-    };
-    // if (
-    //   include.includes(weights[left] / factor(weights[left], weights[right])) &&
-    //   include.includes(weights[right] / factor(weights[left], weights[right]))
-    // ) {
-    //   count++;
-    // } else {
-    //   ++right;
-    // }
-    ++left;
-    // console.log(right, left);
-  }
+  const divi = [1, 2, 3 / 2, 4 / 3];
+  let count = 0;
+  weights
+    .sort((a, b) => b - a)
+    .forEach((el_1, i) => {
+      weights.forEach((_, j) => {
+        return i <= j && divi.includes(el_1 / weights[j + 1]) && count++;
+      });
+    });
   return count;
 }
+
+// 다른 사람 풀이
+// function solution(weights) {
+//   let answer = 0;
+//   const store = {};
+//   const cases = [1, 3/2, 2, 4/3];
+//   weights.sort((a,b)=> b - a).forEach((w) => {
+//       let s;
+//       cases.forEach((i)=>{
+//           if( s = w * i, store[s] ){
+//             answer += store[s];
+//           }
+//         });
+//         if (!store[w]) store[w] = 1;
+//         else store[w]++;
+//   });
+//   return answer;
+// }
 
 console.log(solution(weights));
