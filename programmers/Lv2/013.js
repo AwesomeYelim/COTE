a91d6c9c1bfa803ccc8eb572d5affbec46c2f9aa
Awesomeyelim <uiop01900@gmail.com>
2023-10-23 23:02:56 +0900

    two_pointer🐒

--- programmers/Lv2/013.js
+++ programmers/Lv2/013.js
@@ -1,59 +1,89 @@
 // Lv. 연속된 부분 수열의 합
 
-// const sk = [[1, 2, 3, 4, 5], 7]; // [2, 3]
-// const sk = [[1, 1, 1, 2, 3, 4, 5], 5] // [6, 6]
-const sk = [[2, 2, 2, 2, 2], 6]
+const sk = [[1, 2, 3, 4, 5], 7]; // [2, 3]
+// const sk = [[1, 1, 1, 2, 3, 4, 5], 5]; // [6, 6]
+// const sk = [[2, 2, 2, 2, 2], 6];
 
 // 정답률 44%
-function solution(s, k) {
-  let a = [];
-  if(s.includes(k)) {
-    const h = s.indexOf(k)
-    return [h, h]
-  }
-  for (let i in s) {
-    let p = 0;
-    a[0] = +i;
-    const r = s.slice(i, s.length);
-    for (let j in r) {
-      p += r[j];
-      if (p === k) {
-        a[1] = a[0]+ +j;
-        break;
-      }
-    }
-
-    if (a.length > 1) break;
-  }
-
-  return a;
-}
-
 // function solution(s, k) {
 //   let a = [];
-//   const sorted = [...s].sort((a, b) => b - a);
-//   console.log(s, sorted);
-//   for (let el of sorted) {
-//     let c = k;
-//     c -= el;
-//     const i = s.indexOf(el);
-//     if (!c) {
-//       return [i, i];
-//     } else {
-//       if (i < s.length) {
-//         c -= s[i + 1];
-//         if (c === 0) return (a = [i, i + 1]);
-//         c += s[i + 1];
-//         c -= s[i - 1];
-//         if (c === 0) return (a = [i - 1, i]);
-//       } else {
-//         c -= s[i - 1];
-//         if (!c) return (a = [i - 1, i]);
+//   if(s.includes(k)) {
+//     const h = s.indexOf(k)
+//     return [h, h]
+//   }
+//   for (let i in s) {
+//     let p = 0;
+//     a[0] = +i;
+//     const r = s.slice(i, s.length);
+//     for (let j in r) {
+//       p += r[j];
+//       if (p === k) {
+//         a[1] = a[0]+ +j;
+//         break;
 //       }
 //     }
+
+//     if (a.length > 1) break;
 //   }
 
 //   return a;
 // }
+
+// 정답률 35%
+// function solution(s, k) {
+//   let arr = [];
+//   let l = 0;
+//   let r = 0;
+//   let sum = s[r];
+
+//   if (s.includes(k)) {
+//     const i = s.indexOf(k);
+//     arr = [i, i];
+//     return arr;
+//   }
+//   while (s.length - 1 > r) {
+//     if (sum == k) {
+//       arr = [l, r];
+//       break;
+//     } else if (sum < k) {
+//       r++;
+//       if (l !== r) {
+//         sum += s[r];
+//       }
+//     } else if (sum > k) {
+//       sum -= s[l];
+//       l++;
+//     }
+//   }
+//   return arr;
+// }
+
+function solution(se, k) {
+  var a = [];
+  let p = [0]; 
+  let m = Infinity; 
+  se.forEach((num, i) => {
+    p.push(num + p[i]);
+  });
+
+  let l = 0;
+  let r = 0;
+
+  while (l <= r) {
+    let s = p[r] - p[l];
+    if (s === k) {
+      let c = r - 1 - l;
+      if (m > c) {
+        a = [l, r - 1];
+        m = c;
+      }
+    }
+    if (s < k) {
+      r++;
+    } else l++;
+  }
+
+  return a;
+}
 
 console.log(solution(...sk));
